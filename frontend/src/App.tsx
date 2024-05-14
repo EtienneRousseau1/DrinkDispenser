ee505e25f6df5adc98009c1ab75fa0e775350759
Etienne Rousseau <etar2004@gmail.com>
2024-05-13 23:53:27 -0700

    Updated App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-// ./src/components/DrinkDispenser.tsx
 import React, { useState } from "react";
 import "./App.css";
 
 const DrinkDispenser: React.FC = () => {
   const [drinkSize, setDrinkSize] = useState<number | undefined>(undefined);
+  const [unit, setUnit] = useState<string>("oz");
   const [settings, setSettings] = useState<number[]>([0, 0, 0, 0]);
 
   const handleSettingChange = (index: number, value: number) => {
@@ -14,7 +14,7 @@ const DrinkDispenser: React.FC = () => {
 
   const handleGo = () => {
     console.log("Go pressed");
-    console.log(`Drink Size: ${drinkSize}`);
+    console.log(`Drink Size: ${drinkSize} ${unit}`);
     console.log(`Settings: ${settings}`);
   };
 
@@ -24,17 +24,24 @@ const DrinkDispenser: React.FC = () => {
 
   return (
     <div className="container">
-      <h1 className="header">DrinkDispenser</h1>
-      <label className="label">How big is your drink? (in oz)</label>
-      <input
-        type="number"
-        value={drinkSize}
-        onChange={(e) => setDrinkSize(Number(e.target.value))}
-        className="input"
-      />
+      <h1 className="header">Drink Dispenser</h1>
+      <label className="label">How big is your drink?</label>
+      <div className="input-container">
+        <input
+          type="number"
+          value={drinkSize}
+          onChange={(e) => setDrinkSize(Number(e.target.value))}
+          className="input"
+        />
+        <select value={unit} onChange={(e) => setUnit(e.target.value)} className="input">
+          <option value="oz">oz</option>
+          <option value="ml">ml</option>
+          
+        </select>
+      </div>
       {[0, 1, 2, 3].map((index) => (
         <div key={index} className="mb-2">
-          <label className="label">Setting {index + 1}</label>
+          <label className="label">Liquid {index + 1} (in {unit})</label>
           <input
             type="number"
             value={settings[index]}
