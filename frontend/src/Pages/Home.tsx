3ebfb100e2efd50bbabcc9b99d562bf2c1167150
Colin Chu <colinchu@Colins-MacBook-Pro-6.local>
2024-05-18 17:00:17 -0700

    Changed home page

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -20,8 +20,7 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h2 className="text-4xl font-bold text-center mb-8">Welcome to DUEN's Drink Dispenser!</h2>
-      <p className="text-xl text-center mb-4">Would you like to customize your own drink?</p>
-      <p className="text-l text-slate-400 text-center mb-4">Please input the volume of your cup and then click yes</p>
+      <p className="text-2xl text-slate-400 text-center mb-4">Please input the volume of your cup and then click Go!</p>
       <input 
         type="number" 
         value={volume} 
@@ -34,7 +33,7 @@ const Home: React.FC = () => {
         className={`mt-8 px-6 py-3 rounded ${volume ? 'bg-blue-500 text-white hover:bg-blue-600' : 'bg-gray-500 text-gray-300 cursor-not-allowed'}`}
         disabled={!volume}
       >
-        YES
+        Go!
       </button>
    </div>
   );
